823a561ed82850c2c4dea9d0776168b2f82136bb
Sindhuja Venkatesh <intutivestriker88@gmail.com>
2012-09-15 00:38:51 -0400

    Copyright added to missing files

--- src/internet/model/ipv4-nat.cc
+++ src/internet/model/ipv4-nat.cc
@@ -1,5 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
+ * Copyright (c) 2012 Sindhuja Venkatesh
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -14,6 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * Authors: Sindhuja Venkatesh <intutivestriker88@gmail.com>
  */
 #include "ns3/log.h"
 #include "ns3/uinteger.h"
--- src/internet/model/ipv4-nat.h
+++ src/internet/model/ipv4-nat.h
@@ -1,5 +1,7 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
+ * Copyright (c) 2012 Sindhuja Venkatesh
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation;
@@ -13,6 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * Authors: Sindhuja Venkatesh <intutivestriker88@gmail.com>
  */
 #ifndef IPV4_NAT_H
 #define IPV4_NAT_H
