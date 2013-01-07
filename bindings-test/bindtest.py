71c4bcac8867684d39f4d25363f4d2ff925faed5
Takashi SASAKI <sasaki@cite.ehime-u.ac.jp>
2013-01-07 17:24:08 +0900

    bindtest.py shows all python modules

--- bindings-test/bindtest.py
+++ bindings-test/bindtest.py
@@ -5,19 +5,33 @@ import sys, importlib, pydoc
 print(sys.modules.keys())
 #print(importlib.__import__())
 
-modules= []
+ns3_313_ubuntu_modules = ["antenna", "aodv", "applications", "bridge", "buildings",
+                          "config_store", "core", "csma", "csma_layout",
+                          "dsdv", "dsr", "emu", "energy", "flow_monitor", "internet",
+                          "lte", "mesh", "mobility", "nix_vector_routing",
+                          "olsr", "point_to_point", "point_to_point_layout",
+                          "propagation", "spectrum", "stats", "tap_bridge",
+                          "tools", "topology_read", "uan", "virtual_net_device",
+                          "visualizer", "wifi", "wimax"]
 
+modules = []
 def callback(path, modname, desc, modules=modules):
+    #print("path=%s" % path)
+    print("modname=%s" % modname)
+    #print("desc=%s" % desc)
+    #print("modules=%s" % modules)
+    
 #    if modname and modname[-9:] == ".__init__":
 #        modname = modname[:-9]
 #    if modname.find(".") < 0 and modname not in modules:
 #        modules.append(modname)
+
     if modname not in modules:
         modules.append(modname)
 
 def onerror(modname):
     callback(None, modname, None)
 
-pydoc.ModuleScanner().run(callback)
-print (modules)
+pydoc.ModuleScanner().run(callback, onerror=onerror)
+#print (modules)
 
